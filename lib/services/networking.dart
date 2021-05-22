4d1da11bb4b99f258f792e2a08bc68d4b993606e
skaftisveins <skaftisveins@gmail.com>
2021-05-22 17:17:30 +0000

    Initial commit

---
+++ lib/services/networking.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+
+  final String url;
+
+  Future getData() async {
+    http.Response res = await http.get(url);
+    if (res.statusCode == 200) {
+      String data = res.body;
+
+      return jsonDecode(data);
+    } else {
+      print(res.statusCode);
+    }
+  }
+}
